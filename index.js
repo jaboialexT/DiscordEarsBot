40b6f05f02e0a8a6dee228228c02fce52ff3755e
jaboialexT <Fartypoopybutt@gmail.com>
2023-02-06 18:07:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -350,7 +350,9 @@ function speak_impl(voice_Connection, mapKey) {
 function process_commands_query(txt, mapKey, user) {
   if (txt && txt.length) {
     let val = guildMap.get(mapKey);
-    val.text_Channel.send(user.username + ": " + txt);
+    val.text_Channel.send(
+      user.username + ": " + txt + "\n response: " + chatbot.getReply(txt)
+    );
   }
 }
 
