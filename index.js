f0ef7d557e5c3f8ea45adad6ad8d4e5e0550927a
jaboialexT <Fartypoopybutt@gmail.com>
2023-02-06 19:48:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,32 @@ const { Readable } = require("stream");
 ///////////////// VARIA //////////////////
 //////////////////////////////////////////
 
+const projectId = "jaboialex-nqgq";
+const { Storage } = require("@google-cloud/storage");
+
+async function authenticateImplicitWithAdc() {
+  // This snippet demonstrates how to list buckets.
+  // NOTE: Replace the client created below with the client required for your application.
+  // Note that the credentials are not specified when constructing the client.
+  // The client library finds your credentials using ADC.
+  const storage = new Storage({
+    projectId,
+  });
+  const [buckets] = await storage.getBuckets();
+  console.log("Buckets:");
+
+  for (const bucket of buckets) {
+    console.log(`- ${bucket.name}`);
+  }
+
+  console.log("Listed all storage buckets.");
+}
+
+authenticateImplicitWithAdc();
+
 async function msgReply(textMsg) {
   const sessionId = uuid.v4();
+  const PROJECT_ID = "jaboialex-nqgq";
 
   // Create a new session
   const sessionClient = new dialogflow.SessionsClient();
